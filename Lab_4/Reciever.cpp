de5e7a83a9b90f37b5d7e747738fbde76807bd6d
Медведев Илья <137923678+Medvedial@users.noreply.github.com>
2024-01-03 16:17:24 +0300

    Add files via upload

---
+++ Lab_4/Reciever.cpp
@@ -0,0 +1,101 @@
+﻿#include <fstream> 
+#include <iostream> 
+#include <windows.h> 
+#include <string> 
+
+using namespace std;
+
+void CreateSenderProcesses(const std::string& file_name, int number_of_senders, HANDLE* hEventStarted) {
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    for (int i = 0; i < number_of_senders; ++i) {
+        std::string sender_cmd = "Sender.exe " + file_name;
+
+        LPSTR CommandLine = const_cast<LPSTR>(sender_cmd.c_str());
+        const char* сcommand = (const char*)CommandLine;
+        int len1 = strlen(сcommand) + 1;
+        int size1 = MultiByteToWideChar(CP_UTF8, 0, сcommand, len1, NULL, 0);
+        wchar_t* wCommand = new wchar_t[size1];
+        MultiByteToWideChar(CP_UTF8, 0, сcommand, len1, wCommand, size1);
+
+        ZeroMemory(&si, sizeof(STARTUPINFO));
+        si.cb = sizeof(STARTUPINFO);
+        if (!CreateProcess(NULL, wCommand, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi)) {
+            cout << "The Sender Process is not started." << endl;
+        }
+        hEventStarted[i] = CreateEvent(NULL, FALSE, FALSE, L"StartPocess");
+        CloseHandle(pi.hProcess);
+    }
+}
+
+void HandleMessages(const std::string& file_name, HANDLE hInputReadySemaphore, HANDLE hOutputReadySemaphore, HANDLE hMutex) {
+    std::fstream file;
+    cout << endl << "Enter 1 to read message"
+        << endl << "Enter 0 to exit" << endl;
+    int key;
+    cin >> key;
+    file.open(file_name, std::ios::in);
+
+    while (true) {
+        if (key == 1) {
+            std::string message;
+            WaitForSingleObject(hInputReadySemaphore, INFINITE); //блокирует reciever покак семафор не ьудет освобожден
+            WaitForSingleObject(hMutex, INFINITE);               //блокирует доступ к файлу
+            std::getline(file, message);
+            cout << message;
+            ReleaseSemaphore(hOutputReadySemaphore, 1, NULL);    //освобождает семафор и говорит что reciver может дальше принимать сообщения
+            ReleaseMutex(hMutex);                                //осфобождает мьютекс (дает доступ к файлам)
+            cout << endl << "Enter 1 to read next message"
+                << endl << "Enter 0 to exit" << endl;
+            cin >> key;
+        }
+        if (key != 0 && key != 1) {
+            cout << endl << "Invalid Value"
+                << endl << "Enter 1 to read the next message"
+                << endl << "Enter 0 to exit" << endl;
+            cin >> key;
+        }
+        if (key == 0) {
+            cout << "Process ended";
+            break;
+        }
+    }
+    file.close();
+}
+
+int main() {
+    std::string file_name;
+    int number_of_notes;
+    std::fstream file;
+    int number_of_senders;
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    cout << "Enter binary file name: ";
+    cin >> file_name;
+    cout << "Number of notes: ";
+    cin >> number_of_notes;
+    file.open(file_name, std::ios::out);
+    file.close();
+
+    cout << "Number of processes: ";
+    cin >> number_of_senders;
+
+    HANDLE inputSemaphore = CreateSemaphore(NULL, 0, number_of_notes, L"EnterSemaphoreStarted");
+    HANDLE outputSemaphore = CreateSemaphore(NULL, 0, number_of_notes, L"OutputSemaphoreStarted");
+    HANDLE hMutex = CreateMutex(NULL, 0, L"mut ex");
+    HANDLE* hEventStarted = new HANDLE[number_of_senders];
+
+    CreateSenderProcesses(file_name, number_of_senders, hEventStarted);
+    WaitForMultipleObjects(number_of_senders, hEventStarted, TRUE, INFINITE); //ждет пока не запустятся все процессы sender
+    ReleaseMutex(hMutex); // дает доступ к файлу
+    HandleMessages(file_name, inputSemaphore, outputSemaphore, hMutex);
+
+    CloseHandle(inputSemaphore);
+    CloseHandle(outputSemaphore);
+    for (int i = 0; i < number_of_senders; i++) {
+        CloseHandle(hEventStarted[i]);
+    }
+    delete[] hEventStarted;
+}
---
+++ Lab_4/Sender.cpp
@@ -0,0 +1,95 @@
+﻿#include <fstream> 
+#include <Windows.h> 
+#include <conio.h> 
+#include <iostream> 
+
+using namespace std;
+
+void processMessages(const std::string& file_name, HANDLE hStartEvent, HANDLE hInputReadySemaphore, HANDLE hOutputReadySemaphore, HANDLE hMutex) {
+    std::fstream file;
+    cout << "Press 1 to write the message"
+        << endl << "Write 0 to end the process" << endl;
+    std::string key;
+    cin >> key;
+
+    while (true) {
+        if (key == "1") {
+            WaitForSingleObject(hMutex, INFINITE); //блокирует текущий sender пока не будет получен доступ к файлу
+            file.open(file_name, std::ios::out | std::ios::app);
+
+            std::string msg;
+            cout << "Type your message: ";
+            cin >> msg;
+
+            char message[21];
+            for (int i = 0; i < msg.length(); i++) {
+                message[i] = msg[i];
+            }
+
+            for (int i = msg.length(); i < 21; i++) {
+                message[i] = '\0';
+            }
+
+            message[20] = '\n';
+            ReleaseMutex(hMutex); //дает доступ к файлу
+            ReleaseSemaphore(hOutputReadySemaphore, 1, NULL); //говорит reciever-у что sender записал сообщение и готов принять следующее
+
+            if (ReleaseSemaphore(hInputReadySemaphore, 1, NULL) != 1) {
+                ReleaseSemaphore(hOutputReadySemaphore, 1, NULL);
+                cout << "you have broken the limit of numbers of messages";
+                WaitForSingleObject(hOutputReadySemaphore, INFINITE); //позволяет reciever-у дождаться готовности sender
+                ReleaseSemaphore(hOutputReadySemaphore, 1, NULL); //сообщает reciever что его ждет сообщение
+                ReleaseSemaphore(hInputReadySemaphore, 1, NULL); //говорит reciever что sender готов отправлять сообщение
+
+                for (int i = 0; i < 21; i++) {
+                    file << message[i];
+                }
+            }
+            else {
+                for (int i = 0; i < 21; i++) {
+                    file << message[i];
+                }
+            }
+
+            file.close();
+            cout << endl << "Write 1 to write next message"
+                << endl << "Write 0 to end the process" << endl;
+            cin >> key;
+        }
+
+        if (key != "0" && key != "1") {
+            cout << endl << "Incorrect value"
+                << endl << "Write 1 to write next message"
+                << endl << "Write 0 to end the process" << endl;
+            cin >> key;
+        }
+
+        if (key == "0") {
+            cout << "Process ended";
+            break;
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Usage: " << argv[0] << " <file_name>" << endl;
+        return 1;
+    }
+
+    std::string file_name = argv[1];
+    std::fstream file;
+    HANDLE hStartEvent = OpenEventA(EVENT_MODIFY_STATE, FALSE, "StartPocess");
+    HANDLE hInputReadySemaphore = OpenSemaphore(EVENT_ALL_ACCESS, FALSE, L"EnterSemaphoreStarted");
+    HANDLE hOutputReadySemaphore = OpenSemaphore(EVENT_ALL_ACCESS, FALSE, L"OutputSemaphoreStarted");
+    HANDLE hMutex = OpenMutex(SYNCHRONIZE, FALSE, L"mut ex");
+
+    SetEvent(hStartEvent);
+    cout << "Event was started" << endl;
+
+    processMessages(file_name, hStartEvent, hInputReadySemaphore, hOutputReadySemaphore, hMutex);
+
+    CloseHandle(hInputReadySemaphore);
+    CloseHandle(hOutputReadySemaphore);
+    CloseHandle(hMutex);
+}
